8319a1ea61932076575112425c1f23dd6d318c7c
YANGSANGHEE <109570278+YANGSANGHEE@users.noreply.github.com>
2022-09-15 22:36:56 +0900

    HooksUseEffect

--- hook/src/Component/NamePractice.js
+++ hook/src/Component/NamePractice.js
@@ -1,15 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const NamePractice = () => {
   const [Name, setName] = useState(''); // 변수 초기값 설정
   const [Nickname, setNickname] = useState(''); // 변수 초기값설정
 
+  // useEffect(() => {
+  //   // componentDidmount + componentDidUpdate
+  //   console.log('렌더링됨');
+  //   console.log({
+  //     Name,
+  //     Nickname,
+  //   });
+  // });
+
+  // useEffect(() => {
+  //   console.log('렌더링됨');
+  // }, []); // 마운트만 실행하고 싶을때
+
+  useEffect(() => {
+    //특정값이 업데이트돌 떄만 실행하고 있을때
+    console.log(Name); // Name이 업데이트 될때마다 콘솔찍힘
+  }, [Name]); //두번째 파라미터로 전달되는 배열안에 검사하고 싶은값을 넣어주면 됨
+
   const onChangeName = (e) => {
     setName(e.target.value); // 함수가 호출될시 Name값을 이벤트 타겟의 value값으로 받겠음
-  }
+  };
   const onChangeNicName = (e) => {
     setNickname(e.target.value); // 함수가 호출될시 Name값을 이벤트 타겟의 value값으로 받겠음
-  }
+  };
   return (
     <>
       <div>
@@ -25,7 +43,7 @@ const NamePractice = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default NamePractice
+export default NamePractice;
